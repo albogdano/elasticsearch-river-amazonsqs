77693bda1117c59d5dcb5386306287a11041ea17
Alex B <sashko5@gmail.com>
2012-04-05 20:03:52 +0300

    clean up, upgrade elasticsearch to 0.19.2, update readme.

--- src/main/java/org/elasticsearch/river/amazonsqs/AmazonsqsRiver.java
+++ src/main/java/org/elasticsearch/river/amazonsqs/AmazonsqsRiver.java
@@ -21,17 +21,13 @@ package org.elasticsearch.river.amazonsqs;
 
 import com.amazonaws.AmazonClientException;
 import com.amazonaws.AmazonServiceException;
-
 import com.amazonaws.auth.BasicAWSCredentials;
-
 import com.amazonaws.services.sqs.AmazonSQSAsyncClient;
 import com.amazonaws.services.sqs.model.DeleteMessageRequest;
 import com.amazonaws.services.sqs.model.Message;
 import com.amazonaws.services.sqs.model.ReceiveMessageRequest;
-
 import java.io.IOException;
 import java.util.ArrayList;
-
 import org.elasticsearch.client.Client;
 import org.elasticsearch.action.bulk.BulkResponse;
 import org.elasticsearch.action.bulk.BulkRequestBuilder;
@@ -54,200 +50,187 @@ import org.codehaus.jackson.type.TypeReference;
 */
 public class AmazonsqsRiver extends AbstractRiverComponent implements River {
 
-  private final Client client;
-  private final AmazonSQSAsyncClient sqs;
-  private final ObjectMapper mapper;
+	private final Client client;
+	private final AmazonSQSAsyncClient sqs;
+	private final ObjectMapper mapper;
+	private final String INDEX;
+	private final String ACCESSKEY;
+	private final String SECRETKEY;
+	private final String QUEUE_URL;
+	private final String REGION;
+	private final int MAX_MESSAGES;
+	private final int TIMEOUT;	// in seconds
+	private final int DEFAULT_MAX_MESSAGES = 10;
+	private final int DEFAULT_TIMEOUT = 10;	// in seconds
+	private final String DEFAULT_INDEX = "elasticsearch";
+	
+	private volatile boolean closed = false;
+	private volatile Thread thread;
 
-  private final String INDEX;
-  private final String ACCESSKEY;
-  private final String SECRETKEY;
-  private final String QUEUE_URL;
-  private final String REGION;
-  private final int MAX_MESSAGES;
-  private final int TIMEOUT;
+	@SuppressWarnings({"unchecked"})
+	@Inject
+	public AmazonsqsRiver(RiverName riverName, RiverSettings settings, Client client) {
+		super(riverName, settings);
+		this.client = client;
 
-  private volatile boolean closed = false;
-  private volatile Thread thread;
+		if (settings.settings().containsKey("amazonsqs")) {
+			Map<String, Object> sqsSettings = (Map<String, Object>) settings.settings().get("amazonsqs");
+			REGION = XContentMapValues.nodeStringValue(sqsSettings.get("region"), "null");
+			ACCESSKEY = XContentMapValues.nodeStringValue(sqsSettings.get("accesskey"), "null");
+			SECRETKEY = XContentMapValues.nodeStringValue(sqsSettings.get("secretkey"), "null");
+			QUEUE_URL = XContentMapValues.nodeStringValue(sqsSettings.get("queue_url"), "null");
+		} else {
+			REGION = settings.globalSettings().get("cloud.aws.region");
+			ACCESSKEY = settings.globalSettings().get("cloud.aws.access_key");
+			SECRETKEY = settings.globalSettings().get("cloud.aws.secret_key");
+			QUEUE_URL = settings.globalSettings().get("cloud.aws.sqs.queue_url");
+		}
 
-  @SuppressWarnings({"unchecked"})
+		if (settings.settings().containsKey("index")) {
+			Map<String, Object> indexSettings = (Map<String, Object>) settings.settings().get("index");
+			INDEX = XContentMapValues.nodeStringValue(indexSettings.get("index"), DEFAULT_INDEX);
+			MAX_MESSAGES = XContentMapValues.nodeIntegerValue(indexSettings.get("max_messages"), DEFAULT_MAX_MESSAGES);
+			TIMEOUT = XContentMapValues.nodeIntegerValue(indexSettings.get("timeout_seconds"), DEFAULT_TIMEOUT);
+		} else {
+			INDEX = DEFAULT_INDEX;
+			MAX_MESSAGES = DEFAULT_MAX_MESSAGES;
+			TIMEOUT = DEFAULT_TIMEOUT;
+		}
 
-  @Inject
-  public AmazonsqsRiver(RiverName riverName, RiverSettings settings, Client client) {
-    super(riverName, settings);
-    this.client = client;
+		sqs = new AmazonSQSAsyncClient(new BasicAWSCredentials(ACCESSKEY, SECRETKEY));
+		sqs.setEndpoint("https://".concat(REGION).concat(".queue.amazonaws.com"));
+		mapper = new ObjectMapper();
+	}
 
-    if ( settings.settings().containsKey("amazonsqs") ) {
-      Map<String, Object> sqsSettings = (Map<String, Object>) settings.settings().get("amazonsqs");
+	public void start() {
+		logger.info("creating amazonsqs river using queue {}", QUEUE_URL);
+		thread = EsExecutors.daemonThreadFactory(settings.globalSettings(), "amazonsqs_river").newThread(new Consumer());
+		thread.start();
+	}
 
-      REGION = XContentMapValues.nodeStringValue(sqsSettings.get("region"), "null");
-      ACCESSKEY = XContentMapValues.nodeStringValue(sqsSettings.get("accesskey"), "null");
-      SECRETKEY = XContentMapValues.nodeStringValue(sqsSettings.get("secretkey"), "null");
-      QUEUE_URL = XContentMapValues.nodeStringValue(sqsSettings.get("queue_url"), "null");
+	public void close() {
+		if (closed) {
+			return;
+		}
 
-    } else {
-      REGION = settings.globalSettings().get("cloud.aws.region");
-      ACCESSKEY = settings.globalSettings().get("cloud.aws.access_key");
-      SECRETKEY = settings.globalSettings().get("cloud.aws.secret_key");
-      QUEUE_URL = settings.globalSettings().get("cloud.aws.sqs.queue_url");
+		logger.info("closing amazonsqs river");
+		closed = true;
+		thread.interrupt();
+	}
 
-    }
+	private class Consumer implements Runnable {
 
-    if ( settings.settings().containsKey("index") ) {
-      Map<String, Object> indexSettings = (Map<String, Object>) settings.settings().get("index");
-      INDEX = XContentMapValues.nodeStringValue(indexSettings.get("index"), "elasticsearch");
-      MAX_MESSAGES = XContentMapValues.nodeIntegerValue(indexSettings.get("max_messages"), 10);
-      TIMEOUT = XContentMapValues.nodeIntegerValue(indexSettings.get("timeout_seconds"), 10);
-    } else {
-      INDEX = settings.globalSettings().get("cluster.name");
-      MAX_MESSAGES = 10;
-      TIMEOUT = 10;
-    }
+		private int idleCount = 0;
 
-    sqs = new AmazonSQSAsyncClient(new BasicAWSCredentials(ACCESSKEY, SECRETKEY));
-    sqs.setEndpoint("https://".concat(REGION).concat(".queue.amazonaws.com"));
-    mapper = new ObjectMapper();
-  }
+		public void run() {
+			String id = null;	// document id
+			String type = null;	// document type
+			String indexName = null; // document index
+			Map<String, Object> data = null; // document data for indexing
 
-  public void start() {
-    logger.info("creating amazonsqs river using queue {}", QUEUE_URL);
+			while (!closed) {
+				// pull messages from SQS
+				List<JsonNode> msgs = pullMessages();
+				int sleeptime = TIMEOUT * 1000;
 
-    thread = EsExecutors.daemonThreadFactory(settings.globalSettings(), "amazonsqs_river").newThread(new Consumer());
-    thread.start();
-  }
+				try {
+					BulkRequestBuilder bulkRequestBuilder = client.prepareBulk();
 
-  public void close() {
+					for (JsonNode msg : msgs) {
+						if (msg.has("_id") && msg.has("_type")) {
 
-    if (closed) {
-      return;
-    }
+							id = msg.get("_id").getTextValue();
+							type = msg.get("_type").getTextValue();
+//							//Support for dynamic indexes
+							indexName = msg.has("_index") ? msg.get("_index").getTextValue() : INDEX;
 
-    logger.info("closing amazonsqs river");
-    closed = true;
-    thread.interrupt();
-  }
+							if (msg.has("_data")) {
+								data = mapper.readValue(msg.get("_data"), new TypeReference<Map<String, Object>>() {});
+								bulkRequestBuilder.add(client.prepareIndex(indexName, type, id).setSource(data).request());
+							} else {
+								bulkRequestBuilder.add(client.prepareDelete(indexName, type, id).request());
+							}
+						}
+					}
 
-  private class Consumer implements Runnable {
+					// sleep less when there are lots of messages in queue
+					// sleep more when idle
+					if (bulkRequestBuilder.numberOfActions() > 0) {
 
-    private int idleCount = 0;
+						BulkResponse response = bulkRequestBuilder.execute().actionGet();
+						if (response.hasFailures()) {
+							logger.warn("Bulk operation completed with errors: "
+									+ response.buildFailureMessage());
+						}
 
-    public void run() {
-      String id = null;	// document id
-      String type = null;	// document type
-      String indexName = null; // document index
-      Map<String, Object> data = null; // document data for indexing
+						// many tasks in queue => throttle up
+						if (bulkRequestBuilder.numberOfActions() >= (MAX_MESSAGES / 2)) {
+							sleeptime = 1000;
+						} else if (bulkRequestBuilder.numberOfActions() == MAX_MESSAGES) {
+							sleeptime = 100;
+						}
 
-      while (!closed) {
-        // pull messages from SQS
-        List<JsonNode> msgs = pullMessages();
-        int sleeptime = TIMEOUT * 1000;
+						idleCount = 0;
+					} else {
+						idleCount++;
+						// no tasks in queue => throttle down
+						if (idleCount >= 3) {
+							sleeptime *= 10;
+						}
+					}
+				} catch (Exception e) {
+					logger.error("Bulk index operation failed {}", e);
+					continue;
+				}
 
-        try {
-          BulkRequestBuilder bulkRequestBuilder = client.prepareBulk();
+				try {
+					Thread.sleep(sleeptime);
+				} catch (InterruptedException e) {
+					if (closed) {
+						break;
+					}
+				}
+			}
+		}
 
-          for (JsonNode msg : msgs) {
-            if ( msg.has("_id") && msg.has("_type") ) {
+		private List<JsonNode> pullMessages() {
+			List<JsonNode> msgs = new ArrayList<JsonNode>();
 
-              id = msg.get("_id").getTextValue();
-              type = msg.get("_type").getTextValue();
+			if (!isBlank(QUEUE_URL)) {
+				try {
+					ReceiveMessageRequest receiveReq = new ReceiveMessageRequest(QUEUE_URL);
+					receiveReq.setMaxNumberOfMessages(MAX_MESSAGES);
+					List<Message> list = sqs.receiveMessage(receiveReq).getMessages();
 
-              //Support for dynamic indexes
-              if ( msg.has("_index") ) {
-                indexName = msg.get("_index").getTextValue();
-              } else {
-                indexName = INDEX;
-              }
+					if (list != null && !list.isEmpty()) {
+						for (Message message : list) {
 
-              if ( msg.has("_data") ){
-                data = mapper.readValue(msg.get("_data"), new TypeReference<Map<String, Object>>(){});
-                bulkRequestBuilder.add(client.prepareIndex(indexName, type, id).setSource(data).request());
-              } else {
-                bulkRequestBuilder.add(client.prepareDelete(indexName, type, id).request());
-              }
-            }
-          }
+							if (!isBlank(message.getBody())) {
+								msgs.add(mapper.readTree(message.getBody()));
+							}
 
-          // sleep less when there are lots of messages in queue
-          // sleep more when idle
-          if(bulkRequestBuilder.numberOfActions() > 0) {
+							sqs.deleteMessage(new DeleteMessageRequest(QUEUE_URL, message.getReceiptHandle()));
+						}
+					}
+				} catch (IOException ex) {
+					logger.error(ex.getMessage());
+				} catch (AmazonServiceException ase) {
+					logException(ase);
+				} catch (AmazonClientException ace) {
+					logger.error("Could not reach SQS. {}", ace.getMessage());
+				}
+			}
 
-            BulkResponse response = bulkRequestBuilder.execute().actionGet();
-            if (response.hasFailures()) {
-              logger.warn("Bulk operation completed with errors: " +
-              response.buildFailureMessage());
-            }
+			return msgs;
+		}
 
-            // many tasks in queue => throttle up
-            if (bulkRequestBuilder.numberOfActions() >= (MAX_MESSAGES / 2)) {
-              sleeptime = 1000;
-            } else if (bulkRequestBuilder.numberOfActions() == MAX_MESSAGES) {
-              sleeptime = 100;
-            }
+		private void logException(AmazonServiceException ase) {
+			logger.error("AmazonServiceException: error={}, statuscode={}, " + "awserrcode={}, errtype={}, reqid={}",
+					new Object[]{ase.getMessage(), ase.getStatusCode(), ase.getErrorCode(), ase.getErrorType(), ase.getRequestId()});
+		}
+	}
 
-            idleCount = 0;
-
-          } else {
-            idleCount++;
-            // no tasks in queue => throttle down
-            if( idleCount >= 3 ) {
-              sleeptime *= 10;
-            }
-          }
-        } catch (Exception e) {
-          logger.error("Bulk index operation failed {}", e);
-          continue;
-        }
-
-        try {
-          Thread.sleep(sleeptime);
-        } catch (InterruptedException e) {
-          if (closed) {
-            break;
-          }
-        }
-      }
-    }
-
-    private List<JsonNode> pullMessages() {
-      List<JsonNode> msgs = new ArrayList<JsonNode> ();
-
-      if(!isBlank(QUEUE_URL)){
-
-      try {
-        ReceiveMessageRequest receiveReq = new ReceiveMessageRequest(QUEUE_URL);
-        receiveReq.setMaxNumberOfMessages(MAX_MESSAGES);
-        List<Message> list = sqs.receiveMessage(receiveReq).getMessages();
-
-        if (list != null && !list.isEmpty()) {
-          for (Message message : list) {
-
-            if(!isBlank(message.getBody())) {
-              msgs.add(mapper.readTree(message.getBody()));
-            }
-
-            sqs.deleteMessage(new DeleteMessageRequest(QUEUE_URL, message.getReceiptHandle()));
-          }
-        }
-
-      } catch (IOException ex) {
-        logger.error(ex.getMessage());
-      } catch (AmazonServiceException ase) {
-        logException(ase);
-      } catch (AmazonClientException ace) {
-        logger.error("Could not reach SQS. {}", ace.getMessage());
-      }
-    }
-
-    return msgs;
-  }
-
-  private void logException(AmazonServiceException ase) {
-    logger.error("AmazonServiceException: error={}, statuscode={}, " + "awserrcode={}, errtype={}, reqid={}",
-      new Object[]{ ase.getMessage(), ase.getStatusCode(), ase.getErrorCode(), ase.getErrorType(), ase.getRequestId()});
-    }
-  }
-
-  private boolean isBlank(String str) {
-    return str == null || str.isEmpty() || str.trim().isEmpty();
-  }
-
+	private boolean isBlank(String str) {
+		return str == null || str.isEmpty() || str.trim().isEmpty();
+	}
 }
